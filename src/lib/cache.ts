b992c9c0bdbb8938f354d035009e11640e0e7d92
buck rivard <johrivar@cisco.com>
2021-03-15 19:21:34 -0700

    refactor to use in-mem cache due to API limitations

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,16 +1,16 @@
+const cache = {};
+
 export const getItemFromCache = (key) => {
-  const item = localStorage.getItem(key) || '';
+  const item = cache[key] || '';
 
-  try {
-    const parsed = JSON.parse(item);
-
-    return parsed;
-  } catch {
+  if (item) {
+    return item;
+  } else {
     return null;
   }
 };
 export const setItemInCache = (key, item) => {
-  const itemString = JSON.stringify(item);
 
-  localStorage.setItem(key, itemString);
+  cache[key] = item;
+
 };
